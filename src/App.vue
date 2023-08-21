b7ad7fefadb2e8c1d12748cc8270701d66e4785a
Teguera <aboubacarteguera21@gmail.com>
2023-08-21 23:26:49 +0200

    debut du projet

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+      <h1>hello world</h1>
+  </main>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+
+<style>
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
